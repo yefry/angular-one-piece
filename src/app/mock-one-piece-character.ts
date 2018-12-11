4ead5ccfda2e3bf4b73ef1b02c71406b0f1f3717
yefcode <yefcode@gmail.com>
2018-12-10 20:13:46 -0500

    mock json - adding some characters

--- src/app/mock-one-piece-character.ts
+++ src/app/mock-one-piece-character.ts
@@ -30,6 +30,114 @@ export const ONEPIECECHARACTERLIST: OnePieceCharacter[] = [
         name: 'Jimbei'
     },
     {
+        name: 'Akainu'
+    },
+    {
+        name: 'Aokiji'
+    },
+    {
+        name: 'Arlong'
+    },
+    {
+        name: 'Bartholomew Kuma'
+    },
+    {
+        name: 'Blackbeard'
+    },
+    {
+        name: 'Boa Hancock'
+    },
+    {
+        name: 'Bon Clay'
+    },
+    {
+        name: 'Buggy'
+    },
+    {
+        name: 'Caesar Clown'
+    },
+    {
+        name: 'Cavendish'
+    },
+    {
+        name: 'Coby'
+    },
+    {
+        name: 'Sir Crocodile'
+    },
+    {
+        name: 'Curly Dadan'
+    },
+    {
+        name: 'Django'
+    },
+    {
+        name: 'Dracule Mihawk'
+    },
+    {
+        name: 'Don Quixote Doflamingo'
+    },
+    {
+        name: 'Don Quixote Rocinante'
+    },
+    {
+        name: 'Eneru'
+    },
+    {
+        name: 'Mr. 3'
+    },
+    {
+        name: 'Gecko Moria'
+    },
+    {
+        name: 'Gol D. Roger'
+    },
+    {
+        name: 'Hatchan'
+    },
+    {
+        name: 'Helmeppo'
+    },
+    {
+        name: 'Kizaru'
+    },
+    {
+        name: 'Monkey D. Dragon'
+    },
+    {
+        name: 'Monkey D. Garp'
+    },
+    {
+        name: 'Nefeltari Vivi'
+    },
+    {
+        name: 'Portgaz D. Ace'
+    },
+    {
+        name: 'Sabo'
+    },
+    {
+        name: 'Sengoku'
+    },
+    {
+        name: 'Shanks'
+    },
+    {
+        name: 'Silvers Rayleigh'
+    },
+    {
+        name: 'Smoker'
+    },
+    {
+        name: 'Tashigi'
+    },
+    {
+        name: 'Trafalgar Law'
+    },
+    {
+        name: 'Whitebeard'
+    },
+    {
         name: 'Tony Tony Chopper'
     }
 ];
