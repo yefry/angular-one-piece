d0a54b7e41fd815420bdb51823f3f91e06c15ae9
yefcode <yefcode@gmail.com>
2018-12-22 08:00:28 -0500

    fixing e2e test and adding travis config to run tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-one-piece!');
+    expect(page.getTitleText()).toEqual('One Piece!');
   });
 });
